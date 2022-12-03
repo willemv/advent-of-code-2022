42fd544fa12e59ce3de4b010d55980a3f48b536c
Willem Verstraeten <willem.verstraeten@gmail.com>
2022-12-03 16:32:03 +0100

    day 3

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ mod tests {
     extern crate reqwest;
 
     use std::cmp::Reverse;
+    use std::collections::HashSet;
     use std::env;
     use std::env::VarError;
     use std::error::Error;
+
+    use itertools::Itertools;
 
     fn get_input(day: u8) -> Result<String, Box<dyn Error>> {
         let client = reqwest::blocking::Client::new();
@@ -48,7 +51,6 @@ mod tests {
         );
         Ok(())
     }
-
 
     #[derive(Clone, Copy, Debug)]
     enum Choice {
@@ -117,7 +119,6 @@ mod tests {
 
     #[test]
     fn day2_part1() -> Result<(), Box<dyn std::error::Error>> {
-
         fn parse(c: char) -> Choice {
             match c {
                 'A' | 'X' => Choice::ROCK,
@@ -144,7 +145,6 @@ mod tests {
     }
     #[test]
     fn day2_part2() -> Result<(), Box<dyn std::error::Error>> {
-
         fn parse_opponent(c: char) -> Choice {
             match c {
                 'A' => Choice::ROCK,
@@ -173,13 +173,85 @@ mod tests {
             let required_choice = match expected_outcome {
                 Outcome::WIN => opponent.nemesis(),
                 Outcome::DRAW => opponent,
-                Outcome::LOSS => opponent.sub()
+                Outcome::LOSS => opponent.sub(),
             };
 
             let score = expected_outcome.score() + required_choice.score();
             total_score += score;
         }
         println!("total score: {}", total_score);
+        Ok(())
+    }
+
+    fn day3_priority(c: char) -> u32 {
+        if c.is_ascii_lowercase() {
+            (c as u32) - 97 + 1 //a is ascii code 97
+        } else if c.is_ascii_uppercase() {
+            (c as u32) - 65 + 27 // A is ascii code 65
+        } else {
+            panic!("unsupported char");
+        }
+    }
+
+    #[test]
+    fn day3_part1() -> Result<(), Box<dyn std::error::Error>> {
+        let input = get_input(3)?;
+        let mut priority_sum = 0;
+        for line in input.lines() {
+            let all_chars = line.chars().collect_vec();
+            let mut first_chars_set: HashSet<char> = HashSet::new();
+            let mut second_chars_set: HashSet<char> = HashSet::new();
+            for char in &all_chars[0..all_chars.len() / 2] {
+                first_chars_set.insert(*char);
+            }
+
+            for char in &all_chars[all_chars.len() / 2..] {
+                second_chars_set.insert(*char);
+            }
+
+            let common_chars: Vec<char> =
+                first_chars_set.intersection(&second_chars_set).copied().collect();
+            let common_char = common_chars[0];
+            let priority = day3_priority(common_char);
+            priority_sum += priority;
+        }
+        println!("Total priority: {}", priority_sum);
+        Ok(())
+    }
+
+    #[test]
+    fn day3_part2() -> Result<(), Box<dyn std::error::Error>> {
+        let input = get_input(3)?;
+        let mut priority_sum = 0;
+        for line_triple_iter in &input.lines().chunks(3) {
+            let line_triple = line_triple_iter.collect_vec();
+            let mut first_chars_set: HashSet<char> = HashSet::new();
+            let mut second_chars_set: HashSet<char> = HashSet::new();
+            let mut third_chars_set: HashSet<char> = HashSet::new();
+            for c in line_triple[0].chars().collect_vec() {
+                first_chars_set.insert(c);
+            }
+            for c in line_triple[1].chars().collect_vec() {
+                second_chars_set.insert(c);
+            }
+            for c in line_triple[2].chars().collect_vec() {
+                third_chars_set.insert(c);
+            }
+
+            let first_intersection: HashSet<char> = first_chars_set
+                .intersection(&second_chars_set)
+                .copied()
+                .collect();
+            let common_chars: Vec<char> = first_intersection
+                .intersection(&third_chars_set)
+                .copied()
+                .collect();
+
+            let common_char = common_chars[0];
+            let priority = day3_priority(common_char);
+            priority_sum += priority;
+        }
+        println!("Total priority: {}", priority_sum);
         Ok(())
     }
 }
