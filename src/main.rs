1a6497372a88e34de3abeed9c0ddf2159a8807d6
Willem Verstraeten <willem.verstraeten@gmail.com>
2022-12-05 09:08:44 +0100

    day4

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod tests {
     use std::env;
     use std::env::VarError;
     use std::error::Error;
+    use std::ops::RangeInclusive;
 
     use itertools::Itertools;
 
@@ -209,8 +210,10 @@ mod tests {
                 second_chars_set.insert(*char);
             }
 
-            let common_chars: Vec<char> =
-                first_chars_set.intersection(&second_chars_set).copied().collect();
+            let common_chars: Vec<char> = first_chars_set
+                .intersection(&second_chars_set)
+                .copied()
+                .collect();
             let common_char = common_chars[0];
             let priority = day3_priority(common_char);
             priority_sum += priority;
@@ -252,6 +255,49 @@ mod tests {
             priority_sum += priority;
         }
         println!("Total priority: {}", priority_sum);
+        Ok(())
+    }
+
+    #[test]
+    fn day4() -> Result<(), Box<dyn std::error::Error>> {
+        let input = get_input(4)?;
+
+        fn parse_range(input: &str) -> std::ops::RangeInclusive<u32> {
+            let parts = input.split("-").collect_vec();
+            let start = parts[0].parse::<u32>().unwrap();
+            let end = parts[1].parse::<u32>().unwrap();
+
+            start..=end
+        }
+
+        fn covers(first: &RangeInclusive<u32>, second: &RangeInclusive<u32>) -> bool {
+            second.start() >= first.start() && second.end() <= first.end()
+        }
+
+        fn overlaps(first: &RangeInclusive<u32>, second: &RangeInclusive<u32>) -> bool {
+            !(first.start() > second.end() || first.end() < second.start())
+        }
+
+        let mut num_covering = 0;
+        let mut num_intersecting = 0;
+
+        for line in input.lines() {
+            let groups = line.split(",").collect_vec();
+            let first_range = parse_range(groups[0]);
+            let second_range = parse_range(groups[1]);
+
+            if covers(&first_range, &second_range) || covers(&second_range, &first_range) {
+                num_covering += 1;
+            }
+
+            if overlaps(&first_range, &second_range) {
+                num_intersecting += 1;
+            }
+        }
+
+        println!("Number of fully covering ranges: {}", num_covering);
+        println!("Number of intersecting ranges: {num_intersecting}");
+
         Ok(())
     }
 }
